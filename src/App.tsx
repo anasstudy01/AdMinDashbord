f72ace2c3f749504f479efe33d09889e0f88cc33
anasstudy01 <anasstudy01@gmail.com>
2025-08-11 08:40:52 +0530

    Add router dependency and update styles in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { Button } from "./components/ui/button"
-
+import { Button } from "./components/ui/button";
 
 function App() {
-
-
   return (
     <>
-     <h1>welcome to elib dashbord
+      <div className=" m-4 space-x-3 bg-gray-100">
+        <Button variant={"destructive"}>Click me</Button>
+        <Button variant={"outline"}>Click me</Button>
+      <h1>welcome to elib dashbord</h1>
+      </div>
 
-     </h1>
-
-      <Button variant={"ghost"}>Click me</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
